d3333cc6a249f1297591b479ed9b0f075783e779
Ralph Budiono <ralph.jirehelbudiono@my.jcu.edu.au>
2024-06-12 12:08:22 +0800

    Updated Main Game with player sprite as placeholder

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
+pygame.display.set_caption('Top-Down Race')
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# Load the player image
+player_image = pygame.image.load("Kanata_Tpose.png")
+player_image = pygame.transform.scale(player_image, (60, 77))  # Scale the image to desired size
+
+# Get the rect of the image
+player_rect = player_image.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2))
+
 
 while running:
     # poll for events
@@ -18,19 +26,20 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("green")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # pygame.draw.circle(screen, "black", player_pos, 60)
+    screen.blit(player_image, player_rect.topleft)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_rect.y -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_rect.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_rect.x -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_rect.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
