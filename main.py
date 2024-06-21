8b1431f8c9ad652238de7b1f3391f01ce27130d4
Ralph Budiono <ralph.jirehelbudiono@my.jcu.edu.au>
2024-06-21 17:35:23 +0800

    Added Tree props for the background

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ dt = 0
 
 # Load the game's background
 road_background = pg.image.load('main-game contents/Backgrounds/Road_Background.jpg')
-resized_background = pg.transform.scale(road_background, (1280, 720))
+resized_background = pg.transform.scale(road_background, (1290, 723))
+background_road_pos_y = 0
+tree_props_1 = pg.image.load('main-game contents/Backgrounds/Bunch_of_Trees1.png')
+tree_props_pos_y = -30
 
 
 # Load the player image
 player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player_image = pg.image.load("main-game contents/Vehicles/supercar4.png")
 player_image = pg.transform.scale(player_image, (200, 200))  # Scale the image to desired size
+
+
+def background_tree_props():
+    screen.blit(tree_props_1, (-500, tree_props_pos_y))
+    screen.blit(tree_props_1, (700, tree_props_pos_y))
 
 
 while running:
@@ -28,8 +36,12 @@ while running:
             running = False
 
     # Add the surfaces
-    screen.blit(resized_background, (0, 0))
+    screen.blit(resized_background, (0, background_road_pos_y))
     screen.blit(player_image, player_pos)
+    background_tree_props()
+    tree_props_pos_y += 55
+    if tree_props_pos_y > 200:
+        tree_props_pos_y = -200
 
     keys = pg.key.get_pressed()
     if keys[pg.K_w]:
