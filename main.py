2eba91c13dc74a496d61a1639d4b0b1dfd5e8d19
Jason Elmer Borisman <jasonelmer.borisman@my.jcu.edu.au>
2024-06-23 18:44:22 +0800

    Make the background look lively or moving

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-# pygame setup
+# Pygame setup
 pg.init()
 
 screen = pg.display.set_mode((1280, 720))
@@ -12,10 +12,15 @@ dt = 0
 # Load the game's background
 road_background = pg.image.load('main-game contents/Backgrounds/Road_Background.jpg')
 resized_background = pg.transform.scale(road_background, (1290, 723))
-background_road_pos_y = 0
-tree_props_1 = pg.image.load('main-game contents/Backgrounds/Bunch_of_Trees1.png')
-tree_props_pos_y = -30
 
+# Load the tree props
+tree_props_1 = pg.image.load('main-game contents/Backgrounds/Bunch_of_Trees1.png')
+
+# Initial positions
+background_road_pos_y1 = 0
+background_road_pos_y2 = -723
+tree_props_pos_y1 = 0
+tree_props_pos_y2 = -723
 
 # Load the player image
 player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -23,26 +28,47 @@ player_image = pg.image.load("main-game contents/Vehicles/supercar4.png")
 player_image = pg.transform.scale(player_image, (200, 200))  # Scale the image to desired size
 
 
-def background_tree_props():
-    screen.blit(tree_props_1, (-500, tree_props_pos_y))
-    screen.blit(tree_props_1, (700, tree_props_pos_y))
+def draw_background():
+    screen.blit(resized_background, (0, background_road_pos_y1))
+    screen.blit(resized_background, (0, background_road_pos_y2))
+
+
+def draw_trees():
+    screen.blit(tree_props_1, (-500, tree_props_pos_y1))
+    screen.blit(tree_props_1, (700, tree_props_pos_y1))
+    screen.blit(tree_props_1, (-500, tree_props_pos_y2))
+    screen.blit(tree_props_1, (700, tree_props_pos_y2))
 
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # Poll for events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
     # Add the surfaces
-    screen.blit(resized_background, (0, background_road_pos_y))
+    draw_background()
+    draw_trees()
     screen.blit(player_image, player_pos)
-    background_tree_props()
-    tree_props_pos_y += 55
-    if tree_props_pos_y > 200:
-        tree_props_pos_y = -200
 
+    # Update positions for scrolling effect
+    scroll_speed = 18  # Increase this value to make it faster
+    background_road_pos_y1 += scroll_speed
+    background_road_pos_y2 += scroll_speed
+    tree_props_pos_y1 += scroll_speed
+    tree_props_pos_y2 += scroll_speed
+
+    # Reset positions once off-screen to create continuous loop
+    if background_road_pos_y1 >= 723:
+        background_road_pos_y1 = -723
+    if background_road_pos_y2 >= 723:
+        background_road_pos_y2 = -723
+    if tree_props_pos_y1 >= 723:
+        tree_props_pos_y1 = -723
+    if tree_props_pos_y2 >= 723:
+        tree_props_pos_y2 = -723
+
+    # Player movement
     keys = pg.key.get_pressed()
     if keys[pg.K_w]:
         player_pos.y -= 500 * dt
@@ -53,12 +79,10 @@ while running:
     if keys[pg.K_d]:
         player_pos.x += 300 * dt
 
-    # flip() the display to put your work on screen
+    # Flip the display to put your work on screen
     pg.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    # Limit FPS to 60
     dt = clock.tick(60) / 1000
 
 pg.quit()
